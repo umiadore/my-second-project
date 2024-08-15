b7052924a716b22b3f0c678832b5d867d3a45c1f
umiadore <umi.adore@gmail.com>
2024-08-16 00:26:31 +0400

    7

--- src/main/java/repository/DataRepositoryImpl.java
+++ src/main/java/repository/DataRepositoryImpl.java
@@ -5,7 +5,7 @@ import entity.User;
 import java.util.HashMap;
 import java.util.Map;
 
-// to implement, Probably will be a Map
+// to implement, probably will be a Map
 public class DataRepositoryImpl implements DataRepository{
 
     private Map<String, User> data = new HashMap<>();
